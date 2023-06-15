612fd1c8b5fb1235bb8e0bf96175fb7baef5887a
Victorbankz <olebuv@gmail.com>
2023-06-15 20:52:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,164 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // Add any necessary state variables here
+    // Initialize the state if needed
+  }
+
+  componentDidMount() {
+    // Add any necessary code to run after the component mounts
+  }
+
+  componentDidUpdate() {
+    // Add any necessary code to run after the component updates
+  }
+
+  componentWillUnmount() {
+    // Add any necessary cleanup code here
+  }
+
+  render() {
+    return (
+      <div>
+        {/* Your component's JSX goes here */}
+      </div>
+    );
+  }
+}
+
+
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'John Doe',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        imgSrc: 'path/to/image.jpg',
+        profession: 'Software Engineer'
+      },
+      shows: true
+    };
+  }
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { shows } = this.state;
+
+    return (
+      <div>
+        {shows && (
+          <div>
+            <h1>{fullName}</h1>
+            <p>{bio}</p>
+            <img src={imgSrc} alt="Profile" />
+            <p>{profession}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'John Doe',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        imgSrc: 'path/to/image.jpg',
+        profession: 'Software Engineer'
+      },
+      shows: true
+    };
+  }
+
+  toggleProfile = () => {
+    this.setState(prevState => ({
+      shows: !prevState.shows
+    }));
+  }
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { shows } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.toggleProfile}>
+          {shows ? 'Hide Profile' : 'Show Profile'}
+        </button>
+        {shows && (
+          <div>
+            <h1>{fullName}</h1>
+            <p>{bio}</p>
+            <img src={imgSrc} alt="Profile" />
+            <p>{profession}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'John Doe',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        imgSrc: 'path/to/image.jpg',
+        profession: 'Software Engineer'
+      },
+      shows: true,
+      lastMountedTime: null
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ lastMountedTime: new Date() });
+  }
+
+  componentWillUnmount() {
+    // Perform any necessary cleanup tasks here
+  }
+
+  render() {
+    const { fullName, bio, imgSrc, profession, lastMountedTime } = this.state.person;
+    const { shows } = this.state;
+
+    const currentTime = new Date();
+    const timeInterval = lastMountedTime
+      ? Math.floor((currentTime - lastMountedTime) / 1000) // Time interval in seconds
+      : null;
+
+    return (
+      <div>
+        <button onClick={this.toggleProfile}>
+          {shows ? 'Hide Profile' : 'Show Profile'}
+        </button>
+        {shows && (
+          <div>
+            <h1>{fullName}</h1>
+            <p>{bio}</p>
+            <img src={imgSrc} alt="Profile" />
+            <p>{profession}</p>
+          </div>
+        )}
+        <p>Time since last mount: {timeInterval} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
